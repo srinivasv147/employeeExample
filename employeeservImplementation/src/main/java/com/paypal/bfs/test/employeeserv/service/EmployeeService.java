8ee7449b657b5a06ab45cca00001754528c3df76
Srinivas Vaidyanathan <srinivasv147@gmail.com>
2021-03-22 20:56:53 +0530

    fixed employee service bug

--- employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/service/EmployeeService.java
+++ employeeservImplementation/src/main/java/com/paypal/bfs/test/employeeserv/service/EmployeeService.java
@@ -48,13 +48,15 @@ public class EmployeeService {
 		
 		emp.setId(saved.getId().intValue());
 		
-		emp.getAddress().setCity(saved.getAddress().getCity());
-		emp.getAddress().setState(saved.getAddress().getState());
-		emp.getAddress().setCountry(saved.getAddress().getCountry());
-		emp.getAddress().setZipCode(saved.getAddress().getZipCode());
-		emp.getAddress().setLine1(saved.getAddress().getFirstLine());
-		emp.getAddress().setLine2(saved.getAddress().getSecondLine());
-		emp.getAddress().setId(saved.getAddress().getId().intValue());
+		if(emp.getAddress() != null) {
+			emp.getAddress().setCity(saved.getAddress().getCity());
+			emp.getAddress().setState(saved.getAddress().getState());
+			emp.getAddress().setCountry(saved.getAddress().getCountry());
+			emp.getAddress().setZipCode(saved.getAddress().getZipCode());
+			emp.getAddress().setLine1(saved.getAddress().getFirstLine());
+			emp.getAddress().setLine2(saved.getAddress().getSecondLine());
+			emp.getAddress().setId(saved.getAddress().getId().intValue());
+		}
 		
 	}
 	
